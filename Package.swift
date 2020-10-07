b315992324b25ea2ecc8fd2c58409626d7922780
Joanna Bednarz <joanna.bednarz.jb1@contractors.roche.com>
2020-10-07 10:28:44 +0200

    Make the library dynamic

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftyRSA",
+            type: .dynamic,
             targets: ["SwiftyRSA"]),
     ],
     dependencies: [
