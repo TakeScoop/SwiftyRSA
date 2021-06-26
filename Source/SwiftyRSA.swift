ad0790ea586dd581b427687121ce2e2c315bad79
Nathan Stchepinsky <nathanstchepinsky@protonmail.com>
2021-06-26 11:17:07 +0200

    Support of the X509 header for any public key It fixes  #81 The new method does the exact opposite of the native method named "stripKeyHeader"

--- Source/SwiftyRSA.swift
+++ Source/SwiftyRSA.swift
@@ -3,6 +3,7 @@
 //  SwiftyRSA
 //
 //  Created by Loïs Di Qual on 7/2/15.
+//  Contributions by Stchepinsky Nathan on 24/06/2021
 //  Copyright (c) 2015 Scoop Technologies, Inc. All rights reserved.
 //
 
@@ -283,6 +284,54 @@ public enum SwiftyRSA {
         
         // Unable to extract bit/octet string or raw integer sequence
         throw SwiftyRSAError.invalidAsn1Structure
+    }
+    
+    /**
+        This method prepend the x509 header to the given PublicKey data.
+        If the key already contain a x509 header, the given data is returned as is.
+            It letterally does the opposite of the previous method :
+            From a given headerless key :
+                    SEQUENCE
+                        INTEGER (1024 or 2048 bit) -- modulo
+                        INTEGER -- public exponent
+            the key is returned following the X509 header :
+                    SEQUENCE
+                        SEQUENCE
+                        OBJECT IDENTIFIER 1.2.840.113549.1.1.1
+                        NULL
+                        BIT STRING
+                        SEQUENCE
+                        INTEGER (1024 or 2048 bit) -- modulo
+                        INTEGER -- public exponent
+     */
+    
+    static func prependX509KeyHeader(keyData : Data) throws ->  Data{
+        let node: Asn1Parser.Node
+        do {
+            node = try Asn1Parser.parse(data: keyData)
+        } catch {
+            throw SwiftyRSAError.asn1ParsingFailed
+        }
+        
+        // Ensure the raw data is an ASN1 sequence
+        guard case .sequence(let nodes) = node else {
+            throw SwiftyRSAError.invalidAsn1RootNode
+        }
+        
+        let onlyHasIntegers = nodes.filter { node -> Bool in
+            if case .integer = node {
+                return false
+            }
+            return true
+        }.isEmpty
+        
+        // The key already contains an header
+        if !onlyHasIntegers {
+            return keyData
+        }
+        
+        let x509certificate : Data = keyData.prependx509Header()
+        return x509certificate
     }
     
     static func removeKey(tag: String) {
---
+++ Source/X509Certificate.swift
@@ -0,0 +1,98 @@
+//
+//  X509Certificate.swift
+//  SwiftyRSA
+//
+//  Created by Stchepinsky Nathan on 24/06/2021.
+//  Copyright © 2021 Scoop. All rights reserved.
+//
+
+import Foundation
+
+
+///
+/// Encoding/Decoding lengths as octets
+///
+private extension NSInteger {
+    func encodedOctets() -> [CUnsignedChar] {
+        // Short form
+        if self < 128 {
+            return [CUnsignedChar(self)];
+        }
+        
+        // Long form
+        let i = Int(log2(Double(self)) / 8 + 1)
+        var len = self
+        var result: [CUnsignedChar] = [CUnsignedChar(i + 0x80)]
+        
+        for _ in 0..<i {
+            result.insert(CUnsignedChar(len & 0xFF), at: 1)
+            len = len >> 8
+        }
+        
+        return result
+    }
+    
+    init?(octetBytes: [CUnsignedChar], startIdx: inout NSInteger) {
+        if octetBytes[startIdx] < 128 {
+            // Short form
+            self.init(octetBytes[startIdx])
+            startIdx += 1
+        } else {
+            // Long form
+            let octets = NSInteger(octetBytes[startIdx] as UInt8 - 128)
+            
+            if octets > octetBytes.count - startIdx {
+                self.init(0)
+                return nil
+            }
+            
+            var result = UInt64(0)
+            
+            for j in 1...octets {
+                result = (result << 8)
+                result = result + UInt64(octetBytes[startIdx + j])
+            }
+            
+            startIdx += 1 + octets
+            self.init(result)
+        }
+    }
+}
+
+
+
+extension Data{
+    // This code source come from Heimdall project https://github.com/henrinormak/Heimdall published under MIT Licence
+    
+    /// This method prepend the X509 header to a giver public key
+    func prependx509Header() -> Data {
+        let result = NSMutableData()
+                
+        let encodingLength: Int = (self.count + 1).encodedOctets().count
+        let OID: [CUnsignedChar] = [0x30, 0x0d, 0x06, 0x09, 0x2a, 0x86, 0x48, 0x86,
+                    0xf7, 0x0d, 0x01, 0x01, 0x01, 0x05, 0x00]
+                
+        var builder: [CUnsignedChar] = []
+                
+        // ASN.1 SEQUENCE
+        builder.append(0x30)
+                
+        // Overall size, made of OID + bitstring encoding + actual key
+        let size = OID.count + 2 + encodingLength + self.count
+        let encodedSize = size.encodedOctets()
+        builder.append(contentsOf: encodedSize)
+        result.append(builder, length: builder.count)
+        result.append(OID, length: OID.count)
+        builder.removeAll(keepingCapacity: false)
+                
+        builder.append(0x03)
+        builder.append(contentsOf: (self.count + 1).encodedOctets())
+        builder.append(0x00)
+        result.append(builder, length: builder.count)
+                
+        // Actual key bytes
+        result.append(self)
+                
+        return result as Data
+    }
+}
