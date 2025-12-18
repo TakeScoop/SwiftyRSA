2b3352a71b52c8f34d615870918242fcc90ec82e
Bang Nguyen <bang.nguyen@grabtaxi.com>
2025-12-18 12:36:02 +0800

    Enable LibraryEvolution since the library is stable and avoid ABI breaking issue when an xcframework uses SwiftyRSA

--- Package.swift
+++ Package.swift
@@ -15,6 +15,8 @@ let package = Package(
         .target(
             name: "SwiftyRSA",
             dependencies: [],
-            path: "Source")
+            path: "Source",
+            swiftSettings: [.unsafeFlags(["-enable-library-evolution"])]
+            )
     ]
 )
